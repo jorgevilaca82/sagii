efe7879d8bfe7119d2c03abff1f2cc1c0a51ba82
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-03-13 12:08:21 -0400

    web module

--- sagii/web/urls.py
+++ sagii/web/urls.py
@@ -1,4 +1,4 @@
-"""sagii_web URL Configuration
+"""sagii URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
