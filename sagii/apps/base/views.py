88115eb0f9f14eff9184c7c0c04afb1c3d5a1632
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-03-30 16:01:13 -0400

    formset de documentos no contexto da view

--- sagii/apps/base/views.py
+++ sagii/apps/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
+from django.forms import modelform_factory, formset_factory
 # Create your views here.
 
 from . import models as bm
@@ -33,6 +34,16 @@ class PessoaFisicaCreateView(generic.CreateView):
 
 class PessoaFisicaDetailView(generic.DetailView):
     model = bm.PessoaFisica
+    DocumentoPessoalModelForm = modelform_factory(bm.DocumentoPessoal, fields=('tipo', 'valor', 'observacoes'))
+    DocumentoPessoalFormSet = formset_factory(DocumentoPessoalModelForm, 
+        min_num=bm.DocumentoPessoalTipo.objects.count())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["docs_formset"] = self.DocumentoPessoalFormSet(
+            initial=[{'tipo': tipo} for tipo in bm.DocumentoPessoalTipo.objects.all()])
+        return context
+    
 
 class PessoaFisicaUpdateView(generic.UpdateView):
     model = bm.PessoaFisica
